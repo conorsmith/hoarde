59daa67631eb552717902b89be62f46d044cb2e6
Conor Smith <conorjbsmith@gmail.com>
2019-05-29 14:49:19 +0100

    Refactor entity repo

--- src/Infra/Repository/EntityRepositoryDb.php
+++ src/Infra/Repository/EntityRepositoryDb.php
@@ -10,10 +10,11 @@ use ConorSmith\Hoarde\Domain\ResourceNeed;
 use Doctrine\DBAL\Connection;
 use Ramsey\Uuid\Uuid;
 use Ramsey\Uuid\UuidInterface;
+use Throwable;
 
 final class EntityRepositoryDb implements EntityRepository
 {
-    private const RESOURCE_MAXIMUMS = [
+    private const RESOURCE_MAXIMUMS_FOR_ENTITY = [
         "9972c015-842a-4601-8fb2-c900e1a54177" => 5,
         "6f5cc44d-db25-454a-b3fb-4ab3f61ce179" => 10,
         "5234c112-05be-4b15-80df-3c2b67e88262" => 12,
@@ -33,57 +34,80 @@ final class EntityRepositoryDb implements EntityRepository
 
     public function find(UuidInterface $id): ?Entity
     {
-        $row = $this->db->fetchAssoc("SELECT * FROM entities WHERE id = ?", [
-            strval($id),
+        $row = $this->db->fetchAssoc("SELECT * FROM entities WHERE id = :id", [
+            'id' => $id,
         ]);
 
         if ($row === false) {
             return null;
         }
 
-        $resourceRows = $this->db->fetchAll("SELECT * FROM entity_resources WHERE entity_id = ?", [
-            strval($id),
-        ]);
-
-        if (count($resourceRows) === 0) {
-            return null;
-        }
-
-        $resourceNeeds = [];
-
-        foreach ($resourceRows as $resourceRow) {
-            $resourceNeeds[] = new ResourceNeed(
-                Uuid::fromString($resourceRow['resource_id']),
-                intval($resourceRow['level']),
-                self::RESOURCE_MAXIMUMS[$resourceRow['resource_id']]
-            );
-        }
-
-        $itemRows = $this->db->fetchAll("SELECT * FROM entity_inventory WHERE entity_id = ?", [
-            strval($id),
-        ]);
-
-        $inventory = [];
-
-        foreach ($itemRows as $itemRow) {
-            $item = $this->itemRepository->find(Uuid::fromString($itemRow['item_id']));
-            $item->add(intval($itemRow['quantity']) - 1);
-            $inventory[] = $item;
-        }
-
         return new Entity(
             $id,
             Uuid::fromString($row['game_id']),
             $row['intact'] === "1",
-            $resourceNeeds,
-            $inventory
+            $this->findResourceNeeds($id),
+            $this->findInventory($id)
         );
+    }
+
+    private function findResourceNeeds(UuidInterface $id): iterable
+    {
+        $rows = $this->db->fetchAll("SELECT * FROM entity_resources WHERE entity_id = :id", [
+            'id' => $id,
+        ]);
+
+        $resourceNeeds = [];
+
+        foreach ($rows as $row) {
+            $resourceNeeds[] = new ResourceNeed(
+                Uuid::fromString($row['resource_id']),
+                intval($row['level']),
+                self::RESOURCE_MAXIMUMS_FOR_ENTITY[$row['resource_id']]
+            );
+        }
+
+        return $resourceNeeds;
+    }
+
+    private function findInventory(UuidInterface $id): iterable
+    {
+        $itemRows = $this->db->fetchAll("SELECT * FROM entity_inventory WHERE entity_id = :id", [
+            'id' => $id,
+        ]);
+
+        $inventory = [];
+
+        foreach ($itemRows as $row) {
+            $item = $this->itemRepository->find(Uuid::fromString($row['item_id']));
+            $item->add(intval($row['quantity']) - 1);
+            $inventory[] = $item;
+        }
+
+        return $inventory;
     }
 
     public function save(Entity $entity): void
     {
+        $this->db->beginTransaction();
+
+        try {
+            $this->saveEntity($entity);
+            $this->saveResourceNeeds($entity);
+            $this->saveInventory($entity);
+
+            $this->db->commit();
+
+        } catch (Throwable $e) {
+            $this->db->rollBack();
+            throw $e;
+        }
+    }
+
+    private function saveEntity(Entity $entity): void
+    {
         $row = $this->db->fetchAssoc("SELECT * FROM entities WHERE id = :id", [
-            'id' => strval($entity->getId()),
+            'id' => $entity->getId(),
         ]);
 
         if ($row === false) {
@@ -92,43 +116,40 @@ final class EntityRepositoryDb implements EntityRepository
                 'game_id' => $entity->getGameId(),
                 'intact'  => $entity->isIntact(),
             ]);
-
-            foreach ($entity->getResourceNeeds() as $resourceNeed) {
-                $this->db->insert("entity_resources", [
-                    'entity_id' => $entity->getId(),
-                    'resource_id' => $resourceNeed->getResourceId(),
-                    'level' => $resourceNeed->getCurrentLevel(),
-                ]);
-            }
-
         } else {
             $this->db->update("entities", [
                 'intact' => $entity->isIntact() ? "1" : "0",
             ], [
-                'id' => strval($entity->getId()),
+                'id' => $entity->getId(),
             ]);
-
-            $this->db->delete("entity_resources", [
-                'entity_id' => strval($entity->getId()),
-            ]);
-
-            foreach ($entity->getResourceNeeds() as $resourceNeed) {
-                $this->db->insert("entity_resources", [
-                    'entity_id'   => $entity->getId(),
-                    'resource_id' => $resourceNeed->getResourceId(),
-                    'level'       => $resourceNeed->getCurrentLevel(),
-                ]);
-            }
         }
+    }
 
+    private function saveResourceNeeds(Entity $entity): void
+    {
+        $this->db->delete("entity_resources", [
+            'entity_id' => $entity->getId(),
+        ]);
+
+        foreach ($entity->getResourceNeeds() as $resourceNeed) {
+            $this->db->insert("entity_resources", [
+                'entity_id'   => $entity->getId(),
+                'resource_id' => $resourceNeed->getResourceId(),
+                'level'       => $resourceNeed->getCurrentLevel(),
+            ]);
+        }
+    }
+
+    private function saveInventory(Entity $entity): void
+    {
         $this->db->delete("entity_inventory", [
             'entity_id' => strval($entity->getId()),
         ]);
 
         foreach ($entity->getInventory() as $item) {
             $this->db->insert("entity_inventory", [
-                'entity_id' => strval($entity->getId()),
-                'item_id'   => strval($item->getId()),
+                'entity_id' => $entity->getId(),
+                'item_id'   => $item->getId(),
                 'quantity'  => $item->getQuantity(),
             ]);
         }
