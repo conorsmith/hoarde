e00056b76c2306606eadf11ddc03848db73314cc
Conor Smith <conorjbsmith@gmail.com>
2019-06-07 10:19:08 +0100

    Extract crate template

---
+++ src/Infra/Templates/Game/entity-crate.php
@@ -0,0 +1,44 @@
+<div class="col-lg-4 mb-3">
+    <div class="card">
+        <div class="card-body">
+
+            <h5 class="card-title d-flex justify-content-between">
+                <div>
+                    <i class="fas fa-fw fa-<?=$entity->icon?>"></i>
+                    <?=$entity->label?>
+                </div>
+                <a href="#"
+                   style="display: block; font-size: 1rem; color: #888;"
+                   data-toggle="modal"
+                   data-target="#settingsModal"
+                   data-entity-id="<?=$entity->id?>"
+                >
+                    <i class="fas fa-cog"></i>
+                </a>
+            </h5>
+
+            <p><strong>Inventory</strong></p>
+
+            <?=$this->renderTemplate("Game/inventory.php", [
+                'entity'   => $entity,
+                'isIntact' => $isIntact,
+            ])?>
+
+            <hr>
+
+            <div class="actions">
+
+                <button type="button"
+                        class="btn btn-light btn-block js-transfer"
+                        data-toggle="modal"
+                        data-target="#transferModal"
+                        data-source-id="<?=$entity->id?>"
+                        data-destination-id="<?=$defaultTransferEntity->id?>"
+                    <?=($isIntact ? "" : "disabled")?>
+                >Transfer Items</button>
+
+            </div>
+
+        </div>
+    </div>
+</div>
--- src/Infra/Templates/game.php
+++ src/Infra/Templates/game.php
@@ -198,50 +198,11 @@
 
         <?php foreach ($crates as $crate) : ?>
 
-          <div class="col-lg-4 mb-3">
-            <div class="card">
-              <div class="card-body">
-
-                <h5 class="card-title d-flex justify-content-between">
-                    <div>
-                      <i class="fas fa-fw fa-<?=$crate->icon?>"></i>
-                      <?=$crate->label?>
-                    </div>
-                    <a href="#"
-                       style="display: block; font-size: 1rem; color: #888;"
-                       data-toggle="modal"
-                       data-target="#settingsModal"
-                       data-entity-id="<?=$crate->id?>"
-                    >
-                      <i class="fas fa-cog"></i>
-                    </a>
-                </h5>
-
-                <p><strong>Inventory</strong></p>
-
-                <?=$this->renderTemplate("Game/inventory.php", [
-                    'entity'   => $crate,
-                    'isIntact' => $isIntact,
-                ])?>
-
-                <hr>
-
-                <div class="actions">
-
-                  <button type="button"
-                          class="btn btn-light btn-block js-transfer"
-                          data-toggle="modal"
-                          data-target="#transferModal"
-                          data-source-id="<?=$crate->id?>"
-                          data-destination-id="<?=$entity->id?>"
-                      <?=($isIntact ? "" : "disabled")?>
-                  >Transfer Items</button>
-
-                </div>
-
-              </div>
-            </div>
-          </div>
+            <?=$this->renderTemplate("Game/entity-crate.php", [
+                'entity'                => $crate,
+                'defaultTransferEntity' => $entity,
+                'isIntact'              => $isIntact,
+            ])?>
 
         <?php endforeach ?>
 
