ad8f65ff506a343b6113af31dc36b0069aa2ad7b
Conor Smith <conorjbsmith@gmail.com>
2019-06-11 20:09:16 +0100

    Refactor begin construction methods

--- src/Infra/Controller/HaveEntityConstruct.php
+++ src/Infra/Controller/HaveEntityConstruct.php
@@ -13,7 +13,6 @@ use ConorSmith\Hoarde\Infra\Repository\VarietyRepositoryConfig;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Ramsey\Uuid\Uuid;
-use Ramsey\Uuid\UuidInterface;
 use RuntimeException;
 use Zend\Diactoros\Response;
 
@@ -57,13 +56,102 @@ final class HaveEntityConstruct
         $constructionVarietyId = Uuid::fromString($_POST['constructionVarietyId']);
 
         if ($constructionVarietyId->equals(Uuid::fromString(VarietyRepositoryConfig::WELL))) {
-            return $this->beginConstructingWell($actor, $game);
+            $tools = [
+                VarietyRepositoryConfig::SHOVEL,
+            ];
 
+            $materials = [
+                VarietyRepositoryConfig::ROPE   => 1,
+                VarietyRepositoryConfig::BUCKET => 1,
+            ];
+
+            $constructedEntity = new Entity(
+                Uuid::uuid4(),
+                $game->getId(),
+                Uuid::fromString(VarietyRepositoryConfig::WELL),
+                "Well",
+                "tint",
+                true,
+                new Construction(
+                    false,
+                    9,
+                    10
+                ),
+                [],
+                []
+            );
         } elseif ($constructionVarietyId->equals(Uuid::fromString(VarietyRepositoryConfig::WOODEN_CRATE))) {
-            return $this->beginConstructingCrate($actor, $game);
+            $tools = [
+                VarietyRepositoryConfig::HAMMER,
+                VarietyRepositoryConfig::HAND_SAW,
+            ];
+
+            $materials = [
+                VarietyRepositoryConfig::TIMBER => 10,
+                VarietyRepositoryConfig::NAIL   => 60,
+            ];
+
+            $constructedEntity = new Entity(
+                Uuid::uuid4(),
+                $game->getId(),
+                Uuid::fromString(VarietyRepositoryConfig::WOODEN_CRATE),
+                "Wooden Crate",
+                "box",
+                true,
+                new Construction(
+                    false,
+                    2,
+                    3
+                ),
+                [],
+                []
+            );
+        } else {
+            throw new RuntimeException("Invalid construction");
         }
 
-        throw new RuntimeException("Invalid construction");
+        $meetsRequirements = true;
+
+        foreach ($tools as $tool) {
+            if (!$actor->hasItemInInventory(Uuid::fromString($tool))) {
+                $meetsRequirements = false;
+            }
+        }
+
+        foreach ($materials as $material => $quantity) {
+            if (!$actor->hasItemsAmountingToAtLeast(Uuid::fromString($material), $quantity)) {
+                $meetsRequirements = false;
+            }
+        }
+
+        if (!$meetsRequirements) {
+            $this->session->setFlash("danger", "Construction requirements not met.");
+
+            $response = new Response;
+            $response = $response->withHeader("Location", "/{$game->getId()}");
+            return $response;
+        }
+
+        $this->entityRepo->save($constructedEntity);
+
+        foreach ($materials as $varietyId => $quantity) {
+            $actor->dropItem(Uuid::fromString($varietyId), $quantity);
+        }
+
+        $actor->wait();
+
+        $this->entityRepo->save($actor);
+
+        $game->proceedToNextTurn();
+        $this->gameRepo->save($game);
+
+        if (!$actor->isIntact()) {
+            $this->session->setFlash("danger", "{$actor->getLabel()} has expired");
+        }
+
+        $response = new Response;
+        $response = $response->withHeader("Location", "/{$game->getId()}");
+        return $response;
     }
 
     private function continueConstruction(Entity $actor, Game $game): ResponseInterface
@@ -123,139 +211,6 @@ final class HaveEntityConstruct
 
         $this->entityRepo->save($actor);
         $this->entityRepo->save($target);
-
-        $game->proceedToNextTurn();
-        $this->gameRepo->save($game);
-
-        if (!$actor->isIntact()) {
-            $this->session->setFlash("danger", "{$actor->getLabel()} has expired");
-        }
-
-        $response = new Response;
-        $response = $response->withHeader("Location", "/{$game->getId()}");
-        return $response;
-    }
-
-    private function beginConstructingCrate(Entity $actor, Game $game): ResponseInterface
-    {
-        $tools = [
-            VarietyRepositoryConfig::HAMMER,
-            VarietyRepositoryConfig::HAND_SAW,
-        ];
-
-        $materials = [
-            VarietyRepositoryConfig::TIMBER => 10,
-            VarietyRepositoryConfig::NAIL => 60,
-        ];
-
-        $meetsRequirements = true;
-
-        foreach ($tools as $tool) {
-            if (!$actor->hasItemInInventory(Uuid::fromString($tool))) {
-                $meetsRequirements = false;
-            }
-        }
-
-        foreach ($materials as $material => $quantity) {
-            if (!$actor->hasItemsAmountingToAtLeast(Uuid::fromString($material), $quantity)) {
-                $meetsRequirements = false;
-            }
-        }
-
-        if (!$meetsRequirements) {
-            $this->session->setFlash("danger", "Construction requirements not met.");
-
-            $response = new Response;
-            $response = $response->withHeader("Location", "/{$game->getId()}");
-            return $response;
-        }
-
-        $crate = new Entity(
-            Uuid::uuid4(),
-            $game->getId(),
-            Uuid::fromString(VarietyRepositoryConfig::WOODEN_CRATE),
-            "Wooden Crate",
-            "box",
-            true,
-            new Construction(
-                false,
-                2,
-                3
-            ),
-            [],
-            []
-        );
-
-        $this->entityRepo->save($crate);
-
-        $actor->dropItem(Uuid::fromString(VarietyRepositoryConfig::NAIL), 60);
-        $actor->dropItem(Uuid::fromString(VarietyRepositoryConfig::TIMBER), 10);
-        $actor->wait();
-
-        $this->entityRepo->save($actor);
-
-        $game->proceedToNextTurn();
-        $this->gameRepo->save($game);
-
-        if (!$actor->isIntact()) {
-            $this->session->setFlash("danger", "{$actor->getLabel()} has expired");
-        }
-
-        $response = new Response;
-        $response = $response->withHeader("Location", "/{$game->getId()}");
-        return $response;
-    }
-
-    private function beginConstructingWell(Entity $actor, Game $game): ResponseInterface
-    {
-        $hasShovel = false;
-        $hasRope = false;
-        $hasBucket = false;
-
-        foreach ($actor->getInventory() as $item) {
-            if ($item->getVariety()->getId()->equals(Uuid::fromString(VarietyRepositoryConfig::ROPE))) {
-                $hasRope = true;
-            } elseif ($item->getVariety()->getId()->equals(Uuid::fromString(VarietyRepositoryConfig::BUCKET))) {
-                $hasBucket = true;
-            } elseif ($item->getVariety()->getId()->equals(Uuid::fromString(VarietyRepositoryConfig::SHOVEL))) {
-                $hasShovel = true;
-            }
-        }
-
-        if (!$hasShovel || !$hasRope || !$hasBucket) {
-            $this->session->setFlash(
-                "danger",
-                "Cannot dig a well without a shovel, a rope and a bucket"
-            );
-
-            $response = new Response;
-            $response = $response->withHeader("Location", "/{$game->getId()}");
-            return $response;
-        }
-
-        $well = new Entity(
-            Uuid::uuid4(),
-            $game->getId(),
-            Uuid::fromString(VarietyRepositoryConfig::WELL),
-            "Well",
-            "tint",
-            true,
-            new Construction(
-                false,
-                9,
-                10
-            ),
-            [],
-            []
-        );
-
-        $this->entityRepo->save($well);
-
-        $actor->dropItem(Uuid::fromString(VarietyRepositoryConfig::ROPE), 1);
-        $actor->dropItem(Uuid::fromString(VarietyRepositoryConfig::BUCKET), 1);
-        $actor->wait();
-
-        $this->entityRepo->save($actor);
 
         $game->proceedToNextTurn();
         $this->gameRepo->save($game);
